a53dd6e053139db1714c67ab5ffdfd6ee8e55a4a
Tia <tiallidow@gmail.com>
2025-05-15 16:04:01 -0400

    feat: add operator object methods

---
+++ rpn_calculator/operators.py
@@ -0,0 +1,17 @@
+import operator
+
+""" 
+Defines a catalog for storing and retrieving operator functions based onn their symbols.
+"""
+class OperatorCatalog:
+    def __init__(self):
+        self.operators = {}
+        
+    def add(self, symbol, func):
+        self.operators[symbol] = func
+    
+    def get(self, symbol):
+        return self.operators.get(symbol)
+
+    def check(self, symbol):
+        return symbol in self.operatorss
