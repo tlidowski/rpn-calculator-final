d69f788cc79f8ed44dd544b5b0835ba42ab7f508
Tia <tiallidow@gmail.com>
2025-05-16 11:03:39 -0400

    fix: method calling in main

--- rpn_calculator/rpn_main.py
+++ rpn_calculator/rpn_main.py
@@ -1,4 +1,8 @@
-from rpn_calculator.io_interfaces.cli import run_cli
+from rpn_calculator.io_interfaces.cli import CLICalculator
+
+def run_cli():
+    new_calc = CLICalculator()
+    new_calc.start_cli()
 
 if __name__ == "__main__":
     run_cli()
