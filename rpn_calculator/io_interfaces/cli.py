b4d762cf7a6a0823742101ddcf3afa5faa721c6b
Tia <tiallidow@gmail.com>
2025-05-16 11:55:44 -0400

    feat: add initial exception handling for cli inputs

--- rpn_calculator/io_interfaces/cli.py
+++ rpn_calculator/io_interfaces/cli.py
@@ -12,15 +12,27 @@ class CLICalculator:
     def start_cli(self):
         try:
             self.print_intro()
-            while True: 
+            self.run_calc()
+        except EOFError:
+            self.handle_exit("Received EOF (Ctrl+D). Closing RPN Calculator...")
+        except KeyboardInterrupt:
+            self.handle_exit("Received keyboard interrupt (Ctrl+C). Closing RPN Calculator...")
+        except Exception as e:
+            self.handle_exit(f"Unexpected error: {e}")
+    
+    def run_calc(self):
+        while True:
+            try:
                 num_input = input("> ")
-                if num_input.strip().lower() in ('q'):
-                    print("Closing RPN Calculator...")
-                    break
+                if num_input.strip().lower() == "q":
+                    self.handle_exit("Closing RPN Calculator...")
+                    exit()
                 result = self.calc.evaluate(num_input)
                 print(f"= {result}")
-        except Exception as e:
-            print(f"Error: {e}")
+            except ValueError as ve:
+                self.handle_exit(f"Input error: {ve}")
+            except ZeroDivisionError:
+                self.handle_exit("Math error: Division by zero is not allowed.")
     
     def print_intro(self):
         supported_ops = " ".join(i for i in self.calc.catalog.operators)
@@ -34,3 +46,6 @@ class CLICalculator:
             "Press 'q' or Ctrl+D to exit.\n"
         )
         print("- " * 30)
+
+    def handle_exit(self, message):
+        print(f"\n{message}")
